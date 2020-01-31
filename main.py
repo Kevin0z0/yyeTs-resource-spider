faa031d418507e48a19312a1c8effc5bfabd32d3
Kevin0z0 <644720213@qq.com>
2020-01-31 15:32:41 +0800

    url变动
    官网url变成了www.rrys2019.com，导致爬取错误

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def get_info(html):
 
 def get_score(num): # 获取评分
     try:
-        doc = requests.post("http://www.zmz2019.com/resource/getScore",data={"rid":num},headers=headers).content.decode()
+        doc = requests.post(baseurl + "/resource/getScore",data={"rid":num},headers=headers).content.decode()
         return loads(doc)['score']
     except Exception as e:
         throw_error(e, num)
